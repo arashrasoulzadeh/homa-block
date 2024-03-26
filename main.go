a3cde46f3863c9af2f6cbf8835a5f899c1470382
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2024-03-26 17:51:37 +0330

    [INIT]

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	blockchain := CreateBlockchain(2)
-
-	blockchain.addBlock("Alice", "Bob", 5)
-	blockchain.addBlock("John", "Bob", 2)
-
-	fmt.Println(blockchain.chain)
-
-	fmt.Println(blockchain.isValid())
 }
